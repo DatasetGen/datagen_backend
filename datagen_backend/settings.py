db1b6d9e40805dff4401ca1a99b6e3579e1c9d98
josericardopenase <josericardopenase@gmail.com>
2025-02-13 12:43:44 +0000

    added authorized hosts

--- datagen_backend/settings.py
+++ datagen_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!@gebruc734(76!fc4e=mb3gwj2&z$pxb(c1*iv2ta2x+d=b-v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.22.24']
 
 
 # Application definition
